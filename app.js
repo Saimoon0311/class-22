67800b8760130a5e0e6eaf7775552477b1b99c51
unknown <you@example.com>
2021-06-18 19:06:29 +0500

    class 22

---
+++ app.js
@@ -0,0 +1,49 @@
+// function prs(){
+//     var locat = window.location.href
+//     console.log = (locat)
+// }
+// function locat(){
+//     var locat = window.location.href
+//     window.location.href="https://www.google.com/"
+// }
+// function locat(){
+//     var locat = window.location.href
+//     window.location.assign("https://www.google.com/")
+// }
+// function locat(){
+//     var locate = window.location.hostname
+//     // window.location.href
+//     console.log(locate)
+// }
+// function locat(){
+//     var locat = window.location.href
+//     window.location.replace("https://www.google.com/")
+// }
+// function locat(){
+//     var locat = window.location.href
+//      window.location.reload() // cache reloading
+// }
+// function locat(){
+//     var locat = window.location.href
+//     window.location.reload(true) // server reloading
+// }
+// function locat(){
+//  var hy = '<h1>Susdmkdsksd</h1>'
+//  window.open().document.write(hy)
+// }
+// function locat(){
+//     window.open("https://www.google.com/")
+//    }
+// function locat(){
+//     window.open("https://www.google.com/","name","height=200,width=100")
+//    }
+// function locat(){
+//     window.open("https://www.google.com/","name","height=200,width=100,left=100,top=200")
+//    }
+function locat(){
+   var win = window.open("https://www.google.com/","name","height=200,width=100,left=100,top=200")
+   if(win === null){
+       alert("disable")
+       win.close()
+   }
+}
